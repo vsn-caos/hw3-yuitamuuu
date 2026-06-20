9554ad72cf47c36c50090d9f331cd352bf7e24a6
Елизавета <elizaveta@MacBook-Air-Elizaveta-7.local>
2026-06-20 23:00:13 +0300

    hw3

--- problem3-pipe-connect-2-processes/solution.c
+++ problem3-pipe-connect-2-processes/solution.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-
-// Программе передаётся два аргумента: CMD1 и CMD2.
-// Необходимо запустить два процесса, выполняющих эти команды,
-// и перенаправить стандартный поток вывода CMD1 на стандартный поток ввода CMD2.
-// Эквивалентно: CMD1 | CMD2
-// Родительский процесс должен завершаться самым последним.
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -15,10 +10,76 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // TODO: создайте канал (pipe),
-    //       запустите CMD1 (argv[1]) так, чтобы его stdout → write-конец канала,
-    //       запустите CMD2 (argv[2]) так, чтобы его stdin  ← read-конец канала,
-    //       дождитесь завершения обоих дочерних процессов.
+    int pipe_fds[2];
+
+    if (pipe(pipe_fds) == -1) {
+        perror("pipe");
+        return 1;
+    }
+
+    pid_t first_pid = fork();
+
+    if (first_pid == -1) {
+        perror("fork");
+        close(pipe_fds[0]);
+        close(pipe_fds[1]);
+        return 1;
+    }
+
+    if (first_pid == 0) {
+        if (dup2(pipe_fds[1], STDOUT_FILENO) == -1) {
+            perror("dup2");
+            _exit(1);
+        }
+
+        close(pipe_fds[0]);
+        close(pipe_fds[1]);
+
+        execlp(argv[1], argv[1], (char *) NULL);
+
+        perror("execlp");
+        _exit(1);
+    }
+
+    pid_t second_pid = fork();
+
+    if (second_pid == -1) {
+        perror("fork");
+
+        close(pipe_fds[0]);
+        close(pipe_fds[1]);
+
+        waitpid(first_pid, NULL, 0);
+        return 1;
+    }
+
+    if (second_pid == 0) {
+        if (dup2(pipe_fds[0], STDIN_FILENO) == -1) {
+            perror("dup2");
+            _exit(1);
+        }
+
+        close(pipe_fds[0]);
+        close(pipe_fds[1]);
+
+        execlp(argv[2], argv[2], (char *) NULL);
+
+        perror("execlp");
+        _exit(1);
+    }
+
+    close(pipe_fds[0]);
+    close(pipe_fds[1]);
+
+    if (waitpid(first_pid, NULL, 0) == -1) {
+        perror("waitpid");
+        return 1;
+    }
+
+    if (waitpid(second_pid, NULL, 0) == -1) {
+        perror("waitpid");
+        return 1;
+    }
 
     return 0;
 }
